05a8612964915dd77d1ea0720c2357d6e1efb484
Yang Chen <ychescale9@gmail.com>
2019-08-28 17:09:00 +1000

    Add AppCompat extension for setting precomputed text future. Refactor blueprint-ui extensions.

---
+++ blueprint-ui/src/main/kotlin/reactivecircus/blueprint/ui/extension/AppCompat.kt
@@ -0,0 +1,26 @@
+package reactivecircus.blueprint.ui.extension
+
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.text.PrecomputedTextCompat
+import androidx.core.widget.TextViewCompat
+import java.util.concurrent.Executor
+
+/**
+ * Sets the precomputed text future on the [AppCompatTextView].
+ *
+ * @param charSequence the text to be displayed
+ * @param executor the executor to be used for processing the text layout.
+ *  Default single threaded pool will be used if null is passed in.
+ */
+fun AppCompatTextView.setPrecomputedTextFuture(
+    charSequence: CharSequence,
+    executor: Executor? = null
+) {
+    setTextFuture(
+        PrecomputedTextCompat.getTextFuture(
+            charSequence,
+            TextViewCompat.getTextMetricsParams(this),
+            executor
+        )
+    )
+}
---
+++ blueprint-ui/src/main/kotlin/reactivecircus/blueprint/ui/extension/Context.kt
@@ -0,0 +1,47 @@
+package reactivecircus.blueprint.ui.extension
+
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
+import android.provider.Settings
+import android.util.TypedValue
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
+
+/**
+ * Apply tinting to a vector drawable.
+ */
+fun Context.tintVectorDrawable(
+    theme: Resources.Theme,
+    @DrawableRes resId: Int,
+    @ColorInt tint: Int
+): Drawable {
+    val drawable: Drawable = VectorDrawableCompat.create(resources, resId, theme) as Drawable
+    val wrapped = DrawableCompat.wrap(drawable)
+    drawable.mutate()
+    DrawableCompat.setTint(wrapped, tint)
+    return drawable
+}
+
+/**
+ * Resolves the given color attribute and returns the resource ID associated with the color.
+ */
+@ColorInt
+fun Context.resolveColorAttr(@AttrRes colorAttr: Int): Int {
+    val resolvedAttr = TypedValue()
+    theme.resolveAttribute(colorAttr, resolvedAttr, true)
+    // resourceId is used if it's a ColorStateList, and data if it's a color reference or a hex color
+    val colorRes = if (resolvedAttr.resourceId != 0) resolvedAttr.resourceId else resolvedAttr.data
+    return ContextCompat.getColor(this, colorRes)
+}
+
+/**
+ * Whether animation is turned on on the device.
+ */
+fun Context.isAnimationOn(): Boolean {
+    return Settings.Global.getFloat(contentResolver, Settings.Global.ANIMATOR_DURATION_SCALE, 1f) > 0
+}
