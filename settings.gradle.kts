dcae1c085e8e31a587aa72339bc428010c78cade
Yang Chen <ychescale9@gmail.com>
2019-11-26 22:41:23 +1100

    Migrate settings.gradle to kts.

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "Blueprint"
+
+include(":blueprint-interactor-common")
+include(":blueprint-interactor-coroutines")
+include(":blueprint-interactor-rx2")
+include(":blueprint-interactor-rx3")
+include(":blueprint-threading-coroutines")
+include(":blueprint-threading-rx2")
+include(":blueprint-threading-rx3")
+include(":blueprint-ui")
+include(":blueprint-testing-robot")
+includeProject(":demo-coroutines", "samples/demo-coroutines")
+includeProject(":demo-rx", "samples/demo-rx")
+includeProject(":demo-common", "samples/demo-common")
+includeProject(":demo-testing-common", "samples/demo-testing-common")
+
+fun includeProject(name: String, filePath: String) {
+    include(name)
+    project(name).projectDir = File(filePath)
+}
