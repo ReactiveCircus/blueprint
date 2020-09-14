bae4cf6f4c7b7e7a5e2889c13e1802ea93d406c5
Yang <ychescale9@gmail.com>
2020-09-14 22:48:36 +1000

    Update AppCompatTextView.setPrecomputedTextFuture() to allow passing in null as charSequence.

--- blueprint-ui/src/main/kotlin/reactivecircus/blueprint/ui/extension/AppCompat.kt
+++ blueprint-ui/src/main/kotlin/reactivecircus/blueprint/ui/extension/AppCompat.kt
@@ -8,14 +8,16 @@ import java.util.concurrent.Executor
 /**
  * Sets the precomputed text future on the [AppCompatTextView].
  *
- * @param charSequence the text to be displayed
+ * @param charSequence the text to be displayed.
+ *  Precomputed text future is not set if null is passed in.
  * @param executor the executor to be used for processing the text layout.
  *  Default single threaded pool will be used if null is passed in.
  */
 public fun AppCompatTextView.setPrecomputedTextFuture(
-    charSequence: CharSequence,
-    executor: Executor? = null
+    charSequence: CharSequence?,
+    executor: Executor? = null,
 ) {
+    if (charSequence == null) return
     setTextFuture(
         PrecomputedTextCompat.getTextFuture(
             charSequence,
