d641844a1d8f7fc7e0ed8bec1ee206411eac72ba
Yang <reactivecircus@gmail.com>
2021-02-14 00:00:54 +1100

    AndroidX Core 1.5.0-beta01. Replace deprecated systemUiVisibility usages with WindowInsetsController.

--- blueprint-ui/src/main/kotlin/reactivecircus/blueprint/ui/extension/Activity.kt
+++ blueprint-ui/src/main/kotlin/reactivecircus/blueprint/ui/extension/Activity.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 /**
  * Programmatically close soft keyboard.
@@ -29,40 +31,32 @@ public fun Activity.showKeyboard() {
  * Shows status bar on the activity.
  */
 public fun Activity.showStatusBar() {
-    val decorView = window.decorView
-    val uiOptions = View.SYSTEM_UI_FLAG_VISIBLE
-    decorView.systemUiVisibility = uiOptions
+    WindowInsetsControllerCompat(window, window.decorView.findViewById(android.R.id.content))
+        .show(WindowInsetsCompat.Type.statusBars())
 }
 
 /**
  * Hides status bar from the activity.
  */
 public fun Activity.hideStatusBar() {
-    val decorView = window.decorView
-    val uiOptions = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            or View.SYSTEM_UI_FLAG_FULLSCREEN
-            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
-    decorView.systemUiVisibility = uiOptions
+    WindowInsetsControllerCompat(window, window.decorView.findViewById(android.R.id.content))
+        .hide(WindowInsetsCompat.Type.statusBars())
 }
 
 /**
  * Sets status bar color on the activity and optionally draws the status bar system ui in light or dark mode.
  * @param colorRes resource ID of the color to be set to the status bar.
- * @param lightBackground whether to draw the status bar such that.
+ * @param lightBackground whether to draw the status bar such that
  * it is compatible with a light status bar background.
  */
 public fun Activity.setStatusBarColor(
     @ColorRes colorRes: Int,
-    lightBackground: Boolean = false
+    lightBackground: Boolean = false,
 ) {
     window.statusBarColor = ContextCompat.getColor(this, colorRes)
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        window.decorView.systemUiVisibility = if (lightBackground) {
-            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        } else {
-            0
-        }
+        WindowInsetsControllerCompat(window, window.decorView.findViewById(android.R.id.content))
+            .isAppearanceLightStatusBars = lightBackground
     }
 }
 
@@ -71,8 +65,13 @@ public fun Activity.setStatusBarColor(
  */
 public val Activity.screenSize: DisplayMetrics
     get() {
-        val display = windowManager.defaultDisplay
+        val display = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            display
+        } else {
+            @Suppress("DEPRECATION")
+            windowManager.defaultDisplay
+        }
         val metrics = DisplayMetrics()
-        display.getRealMetrics(metrics)
+        display?.getRealMetrics(metrics)
         return metrics
     }
