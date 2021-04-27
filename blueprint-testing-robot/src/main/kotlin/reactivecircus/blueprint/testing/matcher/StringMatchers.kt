5f47ac261ddebaedcbd033e21b5d84ce48eff89b
Yang <reactivecircus@gmail.com>
2021-04-27 11:31:14 +1000

    Coroutines 1.5.0-RC, RxJava 3.0.12.

--- blueprint-testing-robot/src/main/kotlin/reactivecircus/blueprint/testing/matcher/StringMatchers.kt
+++ blueprint-testing-robot/src/main/kotlin/reactivecircus/blueprint/testing/matcher/StringMatchers.kt
@@ -3,7 +3,6 @@ package reactivecircus.blueprint.testing.matcher
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.TypeSafeMatcher
-import java.util.Locale
 
 /**
  * Returns a matcher that matches string containing a subString (case insensitive).
@@ -12,8 +11,8 @@ public fun containsIgnoringCase(subString: String): Matcher<String> {
 
     return object : TypeSafeMatcher<String>() {
         override fun matchesSafely(actualString: String): Boolean {
-            return actualString.toLowerCase(Locale.getDefault())
-                .contains(subString.toLowerCase(Locale.getDefault()))
+            return actualString.lowercase()
+                .contains(subString.lowercase())
         }
 
         override fun describeTo(description: Description) {
